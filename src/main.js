d2d4ef3b730d4fe8d15883e334ba6f6ebd7c639b
Isaac Eliape <isaaceliape [at] gmail.com>
2020-12-11 23:13:26 +0000

    Added persistence to vuex and load custom fonts on the head tag

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vuex from 'vuex'
+import VueHead from 'vue-head'
+import createPersistedState from "vuex-persistedstate";
 
+import App from './App.vue'
+import store from './Store.js'
+
+Vue.use(VueHead)
+Vue.use(Vuex)
+const app = new Vuex.Store({
+  ...store,
+  plugins: [createPersistedState()],
+});
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store: app,
 }).$mount('#app')
