e52f57fb56bc3aa1c562b812704480e9ac6e5618
Isaac Eliape <isaaceliape@gmail.com>
2021-05-26 20:36:37 +0100

    fixed issue with vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,4 @@
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/typing_tool/' : '/',
-  chainWebpack: config => config.plugin('html').tap(args => args[0].title = "Typing Tool"),
 }
